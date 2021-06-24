a235a5b649a568154731dec4ea3499a5671b73f0
rafaelalbia <rafaelalbia@outlook.com>
2021-06-23 23:58:08 -0300

    Lesson 2 completed

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
+import "reflect-metadata";
 import express from 'express';
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send("Hello World!"); 
-});
+app.use(express.json())
 
-app.post("/test-post", (request, response) => {
-  return response.send("Hello, World!");
-});
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running"));
